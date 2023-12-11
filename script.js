b54cfbac801d42ed4c67202c3124bc87997c4da6
chandruMurthy <113250768+chaand2321@users.noreply.github.com>
2023-12-11 15:59:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-//your JS code here. If required.
+// // //your JS code here. If required.
+// // // var fn=document.getElementById(fn);
+// // // var aj=document.getElementById(aj);
+
+// // // fn.addEventListener( "click",functionName(){ alert("functionName"); } );
+// // // aj.addEventListener( "click",AccioJob(){ alert("AccionJob"); } );
+
+// // AccioJob(){ alert("AccionJob") };
+// // functionName(){ alert("functionName") };
+// function functionName() {
+//     alert("functionName");
+// }
+// function AcciJob() {
+//     alert("AccioJob");
+// }
+// // Example usage
+// functionName();
+// AccioJob();
+// // This will display an alert with the text "functionName"
+// Function named functionName
+function functionName() {
+    alert("functionName");
+}
+
+// Function named AccioJob
+function AccioJob() {
+    alert("AccioJob");
+}
+
+// Example usage
+functionName(); // This will display an alert with the text "functionName"
+AccioJob();    // This will display an alert with the text "AccioJob"
